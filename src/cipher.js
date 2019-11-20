b8ef51f388eab4e9c76b6bfcf33b7722f46a5f05
PauletteOrdenes <paulette1975xx@gmail.com>
2019-11-20 19:53:19 -0300

    Arreglando función validar en app.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,50 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
+  encode: (number, text) => {
+
+    let resultado = ''; // variable vacía para el resultado
+    for (let i = 0; i < text.length; i++) {
+      let txt = text.charCodeAt(i); // saca codigo ASCII de las letras
+      // console.log(txt);
+
+      if (txt >= 65 && txt <= 90) {
+        let formulaAscii = (txt - 65 + number) % 26 + 65;
+        let letraCifrada = String.fromCharCode(formulaAscii); // pasa el numero ASCII a letra
+        // console.log(letraCifrada);
+        resultado += letraCifrada;
+      } else if (txt >= 97 && txt <= 122) { // para minusculas
+        let formulaAscii = (txt - 97 + number) % 26 + 97;
+        let letraCifrada = String.fromCharCode(formulaAscii);
+        // console.log(letraCifrada);
+        resultado += letraCifrada;
+      } else { // para todo lo demás
+        resultado += text.charAt(i);
+      }
+
+    }
+    return resultado;
   },
-  decode: () => {
-    /* Acá va tu código */
+
+  decode: (number, text) => {
+    let resultado='';
+    for(let i = 0; i < text.length; i++){
+      let txtascii = text.charCodeAt(i)
+      console.log(txtascii)
+      if(txtascii >= 65 && txtascii <= 90){
+        console.log("hola")
+        let formulaAscii = (txtascii-90-number) % 26 +  90;
+        let letraDescifrada = String.fromCharCode(formulaAscii);
+        resultado += letraDescifrada;
+      } else if (txtascii >= 97 && txtascii <= 122){
+        let formulaAscii = (txtascii - 122 - number) % 26 + 122;
+        let letraDescifrada = String.fromCharCode (formulaAscii);
+        console.log (letraDescifrada);
+        resultado += letraDescifrada;
+      } else {
+        resultado += text.charAt(i);
+      }
+     
+    }
+    return resultado;
+
   }
-};
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-/*Acá va tu código*/
+
+
+let btnCod= document.getElementById('btnCod'); // variable que llama a los valores del botón
+btnCod.addEventListener('click', () =>{
+    let offset= document.getElementById('offset').value; // agarra valor de offset
+    console.log(offset)
+    let text= document.getElementById('ingresarTxt').value; // agarra valor de textarea
+    // console.log(text)
+    // let letras= text.toUpperCase(); // convierte valor de textarea en mayusculas 
+    let number = parseInt(offset); // convierte offset en numero para hacer conversión
+    document.getElementById('textoSalida').innerHTML= window.cipher.encode(number, text);
+});
+
+let btnDec = document.getElementById('btnDec');
+btnDec.addEventListener('click', ()=>{
+    let offset= document.getElementById('offset').value;
+    // console.log(offset);
+    let text= document.getElementById('ingresarTxt').value; 
+    // console.log (text); 
+    let number = parseInt(offset);
+    document.getElementById('textoSalida').innerHTML=window.cipher.decode(number, text);
+});
+
+
+
+
